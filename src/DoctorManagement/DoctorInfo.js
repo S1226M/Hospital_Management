376b04b98a3f8da54f895220c033697e93074699
S1226M <smitmaru1226@gmail.com>
2024-12-03 21:28:46 +0530

    03-12-2024

--- src/DoctorManagement/DoctorInfo.js
+++ src/DoctorManagement/DoctorInfo.js
@@ -1,18 +1,30 @@
 import React from "react";
 
 function DoctorInfo(){
+
+    const formattedDoctors = 
+
     return(
         <div className="doctorInfoContainer">zccddc
-            <h2 className="doctorInfo">Doctor Information</h2>
-            <table>
-                <tr>
-                    <th>No.</th>
-                    <th>Full Name</th>
-                    <th>Specialization</th>
-                    <th>Phone Number</th>
-                    <th>Email Id</th>
-                    <th>Availability</th>
-                </tr>
+            <h2 className="doctorInfoHeading">Doctor Information</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        
+                        <th>No.</th>
+                        <th>Name</th>
+                        <th>Department</th>
+                        <th>Phone Number</th>
+                        <th>Specialization</th>
+                        <th>Experience</th>
+                        <th>Education</th>
+                        <th>Email Id</th>
+                        <th>Time</th>
+                        <th>Availability</th>
+
+                    </tr>
+                </thead>
+                <tbody>{formattedDoctors}</tbody>
             </table>
         </div>
     )
