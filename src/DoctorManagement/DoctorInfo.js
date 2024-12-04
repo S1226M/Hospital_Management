ff82d90feab267e07d7c87bff9c67fce128a21c8
S1226M <smitmaru1226@gmail.com>
2024-12-04 22:44:24 +0530

    04-12-2024

--- src/DoctorManagement/DoctorInfo.js
+++ src/DoctorManagement/DoctorInfo.js
@@ -2,32 +2,32 @@ import React from "react";
 
 function DoctorInfo(){
 
-    const formattedDoctors = 
+    // const formattedDoctors = 
 
-    return(
-        <div className="doctorInfoContainer">zccddc
-            <h2 className="doctorInfoHeading">Doctor Information</h2>
-            <table className="table">
-                <thead>
-                    <tr>
+    // return(
+    //     <div className="doctorInfoContainer">zccddc
+    //         <h2 className="doctorInfoHeading">Doctor Information</h2>
+    //         <table className="table">
+    //             <thead>
+    //                 <tr>
                         
-                        <th>No.</th>
-                        <th>Name</th>
-                        <th>Department</th>
-                        <th>Phone Number</th>
-                        <th>Specialization</th>
-                        <th>Experience</th>
-                        <th>Education</th>
-                        <th>Email Id</th>
-                        <th>Time</th>
-                        <th>Availability</th>
+    //                     <th>No.</th>
+    //                     <th>Name</th>
+    //                     <th>Department</th>
+    //                     <th>Phone Number</th>
+    //                     <th>Specialization</th>
+    //                     <th>Experience</th>
+    //                     <th>Education</th>
+    //                     <th>Email Id</th>
+    //                     <th>Time</th>
+    //                     <th>Availability</th>
 
-                    </tr>
-                </thead>
-                <tbody>{formattedDoctors}</tbody>
-            </table>
-        </div>
-    )
+    //                 </tr>
+    //             </thead>
+    //             <tbody>{formattedDoctors}</tbody>
+    //         </table>
+    //     </div>
+    // )
 }
 
 export default DoctorInfo;
--- src/Login&Logout/SignUp.js
+++ src/Login&Logout/SignUp.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './SignIn&SignUp.css';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function SignUp() {
+  const [data , setData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  })
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle sign-up logic here
+    axios.post('/*url from server side*/',{name, email, password})
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
   };
 
   return (
@@ -16,9 +24,9 @@ function SignUp() {
         <h2 className="form-title">Sign-Up</h2>
         <p>Welcome! 😊</p>
         <form onSubmit={handleSubmit} className="auth-form">
-          <input type="text" placeholder="Full Name" className="form-input" />
-          <input type="email" placeholder="Email Address" className="form-input" />
-          <input type="password" placeholder="Password" className="form-input" />
+          <input type="text" placeholder="Full Name" className="form-input" onChange={(e)=>setData({ ...data, fullName: e.target.value })} />
+          <input type="email" placeholder="Email Address" className="form-input" onChange={(e)=>setData({ ...data, email: e.target.value })} />
+          <input type="password" placeholder="Password" className="form-input" onChange={(e)=>setData({ ...data, password: e.target.value })} />
           <button type="submit" className="btn primary-btn" onClick={() => navigate('/layout')}>Sign Up</button>
         </form>
         <div className="form-footer">
