ec0cf5749d903803019bf7c41ebc200f4e42fd65
S1226M <smitmaru1226@gmail.com>
2025-01-03 19:44:28 +0530

    03-01-2025

--- src/Login&Logout/ForgotPassword.js
+++ src/Login&Logout/ForgotPassword.js
@@ -1,22 +1,28 @@
 import React from "react";
 import { FaCircleExclamation } from "react-icons/fa6";
 import { IoMail } from "react-icons/io5";
+import './SignIn&SignUp.css'
 
-function ForgotPassword(){
-    return(
-        <>
-            <h1>ForgotPassword</h1>
+function ForgotPassword() {
+    return (
+      <>
+        <div className="outer-box">
+          <form>
             <div className="forgotPassword-container">
-                <div className="icon"><FaCircleExclamation /></div>
-                <p className="msg-forgotPassword">Enter your email and we'll send you a code and enter code to reset your password</p>
-                <div className="email">
-                    <IoMail />
-                    <input type="email" placeholder="Enter your email" />
-                </div>
-                <button>Submit</button>
+              <div className="icon"><FaCircleExclamation /></div>
+              <p className="msg-forgotPassword">
+                Enter your email and we'll send you a code and enter the code to reset your password
+              </p>
+              <div className="email">
+                <IoMail />
+                <input type="email" placeholder="Enter your email" />
+              </div>
+              <button>Submit</button>
             </div>
-        </>
-    )
-}
-
+          </form>
+        </div>
+      </>
+    );
+  }
+  
 export default ForgotPassword;
